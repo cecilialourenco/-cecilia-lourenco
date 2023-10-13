6254b6b640713d52db8b5eeec469e052d3e1bfca
Cecilia Lourenco <ceciliamblourenco@gmail.com>
2023-10-13 18:37:42 -0300

    create Menu component

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,13 @@
+const Menu = () => {
+  return (
+    <nav className="flex flex-row space-x-10 absolute top-10 end-5">
+      <a href="#sobreMim">SOBRE</a>
+      <a href="#experiencia">EXPERIÊNCIA</a>
+      <a href="#projetos">PROJETOS</a>
+      <a href="#objetivos">OBJETIVOS</a>
+    </nav>
+   
+  );
+};
+
+export default Menu;
