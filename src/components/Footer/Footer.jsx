d477b42932a55ef2317e98a9f3d514d7a7b23e91
Cecilia Lourenco <ceciliamblourenco@gmail.com>
2023-10-16 17:12:21 -0300

    create Footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return(
+    <footer>
+      <p className='text-center p-10 bg-neutral-200 h-[100px]'>©Copyright 2023. Made by Cecília Lourenço</p>
+    </footer>
+  );
+};
+
+export default Footer;
