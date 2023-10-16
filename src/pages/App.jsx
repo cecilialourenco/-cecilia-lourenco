b130c43ddc2fcc852ef53d5632b72594e58c859b
Cecilia Lourenco <ceciliamblourenco@gmail.com>
2023-10-16 17:14:22 -0300

    update App

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,7 +2,9 @@ import Experiencia from '../components/Experiencia/Experiencia';
 import Header from '../components/Header/Header';
 import Sobre from '../components/Sobre/Sobre';
 import Projetos from '../components/Projetos/Projetos';
-import Objetivos from '../components/Objetivos/Objetivos';
+import Contatos from '../components/Contatos/Contatos';
+import Home from '../components/Home/Home';
+import Footer from '../components/Footer/Footer';
 
 const App = () => {
   return (
@@ -10,10 +12,12 @@ const App = () => {
         <Header />
       <div className="grow relative pt-[100px]">
         <img className='h-full w-full fixed z-[-10] opacity-40' src='/design-elegante-de-fundo-abstrato-de-memphis.jpg'/>
+        <Home />
         <Sobre />
         <Experiencia />
         <Projetos />
-        <Objetivos />
+        <Contatos />
+        <Footer />
       </div> 
     </div>
   )    
