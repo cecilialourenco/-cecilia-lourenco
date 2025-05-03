4f3010085e6394b7a29cda69dd28e7b8d6945b93
Cecilia Lourenco <ceciliamblourenco@gmail.com>
2025-05-02 21:22:54 -0300

    Update information about me

--- src/components/Sobre/Sobre.jsx
+++ src/components/Sobre/Sobre.jsx
@@ -5,11 +5,19 @@ const Sobre = () => {
       <div className='flex items-center max-w-[1600px] mx-20' >
         <img src='/fotoAtual.jpg' className='h-[300px] w-[250px] pr-5 rounded-t-full lg:block hidden' />   
         <p className='lg:leading-relaxed text-justify text-xl lg:text-3xl text-slate-950'>
-          Meu nome é Cecília, tenho 40 anos e moro na cidade do Rio de Janeiro.
-          Estou cursando graduação em Análise e Desenvolvimento de Sistemas, na
-          modalidade EAD, com previsão de formatura para abril de 2025. 
-          Resolvi criar esta página para servir de portfólio e contar sobre a minha 
-          transição de carreira de Psicóloga para Desenvolvedora Web.
+          Meu nome é Cecília, tenho 41 anos e moro na cidade do Rio de Janeiro.
+          Sou uma profissional em transição para Gerência de Projetos em TI, combinando 
+          3 anos de experiência em tecnologia com 10 anos de Psicologia social/
+          institucional em ambientes de alta complexidade.<br></br><br></br>
+
+          Minha trajetória me capacitou a gerenciar stakeholders conflitantes, 
+          mediando necessidades técnicas, operacionais e humanas; traduzir 
+          demandas não-técnicas em requisitos claros para equipes de 
+          desenvolvimento (graças ao duplo background) e otimizar processos com 
+          foco em usabilidade e eficiência, sempre aliado a práticas ágeis.<br></br><br></br>
+
+          Busco oportunidades para aplicar essa visão única em projetos de TI, 
+          unindo gestão ágil, resiliência organizacional e foco no usuário.
         </p>
       </div>
     </section>
